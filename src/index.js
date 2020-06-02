25af599cbde9be76701af226b3ee75002f7457ec
Rafael Gil <rafaelgil83@gmail.com>
2020-06-02 11:10:53 -0400

    - added close functions

--- src/index.js
+++ src/index.js
@@ -6,10 +6,38 @@ let mainWindow;
 
 //listen for app to be ready
 
-app.on('ready', () => {
-    //create new window
-    mainWindow = new BrowserWindow({});
+function createWindow () {
+    // Create the browser window.
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    })
 
-    //load the html for the main window
-    mainWindow.loadFile('src/mainWindow.html');
+    // and load the index.html of the app.
+    win.loadFile('src/mainWindow.html')
+
+    // Open the DevTools.
+    // win.webContents.openDevTools()
+}
+
+app.whenReady().then(createWindow);
+
+// Quit when all windows are closed.
+app.on('window-all-closed', () => {
+    // On macOS it is common for applications and their menu bar
+    // to stay active until the user quits explicitly with Cmd + Q
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+})
+
+app.on('activate', () => {
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow()
+    }
 })
