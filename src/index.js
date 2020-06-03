1def1bc4d6bb8b2bcfb27dba7015b706a247e26d
Rafael Gil <rafaelgil83@gmail.com>
2020-06-03 10:05:49 -0400

    - refactored entry file

--- src/index.js
+++ src/index.js
@@ -1,189 +1,36 @@
 const electron = require('electron');
 
 const {app, BrowserWindow, Menu, ipcMain, Tray} = electron;
+const WindowController = require('./lib/WindowController');
+
+const windowController = new WindowController(BrowserWindow, app);
 
 let mainWindow, settingsWindow, tray, timer;
 const isMac = process.platform === 'darwin';
+const timerDuration = 40; //in minutes
 
 // process.env.NODE_ENV = 'production';
-
-function createMainWindow() {
-    // Create the browser window.
-    mainWindow = new BrowserWindow({
-        width: 800,
-        height: 600,
-        alwaysOnTop: true,
-        fullscreen: true,
-        modal: true,
-        webPreferences: {
-            nodeIntegration: true
-        }
-    })
-
-    // and load the index.html of the app.
-    mainWindow.loadFile('src/mainWindow.html');
-    mainWindow.on('closed', app.quit);
-
-    // Open the DevTools.
-    if (process.env.NODE_ENV !== 'production') {
-        mainWindow.webContents.openDevTools();
-    }
-}
-
-function createMainMenu() {
-
-    //if MAC, add empty object to menu
-    if (process.platform === 'darwin') {
-        // mainMenuTemplate.unshift({})
-    }
-
-    let mainMenuTemplate = [
-        // { role: 'appMenu' }
-        ...(isMac ? [{
-            label: app.name,
-            submenu: [
-                {role: 'about'},
-                {type: 'separator'},
-                {role: 'services'},
-                {type: 'separator'},
-                {role: 'hide'},
-                {role: 'hideothers'},
-                {role: 'unhide'},
-                {type: 'separator'},
-                {role: 'quit'}
-            ]
-        }] : []),
-        // { role: 'fileMenu' }
-        {
-            label: 'File',
-            submenu: [
-                isMac ? {role: 'close'} : {role: 'quit'},
-                {
-                    label: 'Settings',
-                    click() {
-                        createSettingsWindow()
-                    }
-                }
-            ],
-        },
-        // { role: 'editMenu' }
-        {
-            label: 'Edit',
-            submenu: [
-                {role: 'undo'},
-                {role: 'redo'},
-                {type: 'separator'},
-                {role: 'cut'},
-                {role: 'copy'},
-                {role: 'paste'},
-                ...(isMac ? [
-                    {role: 'pasteAndMatchStyle'},
-                    {role: 'delete'},
-                    {role: 'selectAll'},
-                    {type: 'separator'},
-                    {
-                        label: 'Speech',
-                        submenu: [
-                            {role: 'startspeaking'},
-                            {role: 'stopspeaking'}
-                        ]
-                    }
-                ] : [
-                    {role: 'delete'},
-                    {type: 'separator'},
-                    {role: 'selectAll'}
-                ])
-            ]
-        },
-        // { role: 'viewMenu' }
-        {
-            label: 'View',
-            submenu: [
-                {role: 'reload'},
-                {role: 'forcereload'},
-                {role: 'toggledevtools'},
-                {type: 'separator'},
-                {role: 'resetzoom'},
-                {role: 'zoomin'},
-                {role: 'zoomout'},
-                {type: 'separator'},
-                {role: 'togglefullscreen'}
-            ]
-        },
-        // { role: 'windowMenu' }
-        {
-            label: 'Window',
-            submenu: [
-                {role: 'minimize'},
-                {role: 'zoom'},
-                ...(isMac ? [
-                    {type: 'separator'},
-                    {role: 'front'},
-                    {type: 'separator'},
-                    {role: 'window'}
-                ] : [
-                    {role: 'close'}
-                ])
-            ]
-        },
-        {
-            role: 'help',
-            submenu: [
-                {
-                    label: 'Learn More',
-                    click: async () => {
-                        const {shell} = require('electron')
-                        await shell.openExternal('https://electronjs.org')
-                    }
-                }
-            ]
-        }
-    ]
-
-    //Add developer tools item if not in prod
-    if (process.env.NODE_ENV !== 'production') {
-        mainMenuTemplate.push({
-            label: "Developer Tool",
-            submenu: [
-                {
-                    label: "Toggle DevTools",
-                    accelerator: isMac ? 'Command+I' : 'Ctrl+I',
-                    click(item, focusedWindow) {
-                        focusedWindow.toggleDevTools();
-                    }
-                }
-            ]
-        });
-    }
-
-    const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
-    Menu.setApplicationMenu(mainMenu);
-}
-
-function createSettingsWindow() {
-    settingsWindow = new BrowserWindow({
-        width: 300,
-        height: 200,
-        webPreferences: {
-            nodeIntegration: true
-        }
-    })
-
-    // and load the index.html of the app.
-    settingsWindow.loadFile('src/views/settingsWindow.html')
-
-    settingsWindow.on('close', () => {
-        settingsWindow = null;
-    })
-}
 
 function createTray() {
     tray = new Tray('src/assets/img/icons/tray_icons/IconTemplate.png')
     const contextMenu = Menu.buildFromTemplate([
         {
             label: 'Open', click: async () => {
-                createMainWindow();
-                createMainMenu();
+                windowController.createSettingsWindow()
+            }
+        },
+        {type: 'separator'},
+        {
+            label: "Start break",
+            click: async () => {
+                windowController.createBreakWindow();
+            }
+        },
+        {type: 'separator'},
+        {
+            label: 'Quit',
+            click() {
+                app.quit();
             }
         }
     ])
@@ -193,7 +40,7 @@ function createTray() {
 
 function startTimer() {
 
-    let duration = 1 * 60 * 1000;
+    let duration = timerDuration * 60 * 1000;
     let countDownDate = new Date(Date.now() + (duration)).getTime();
     let i = 0;
 
@@ -216,7 +63,7 @@ function startTimer() {
 
         if (distance <= 1) {
             clearInterval(timer);
-            createMainWindow();
+            windowController.createBreakWindow();
         } else {
             tray.setTitle(`${minutes}:${seconds}`);
         }
@@ -243,13 +90,13 @@ app.on('window-all-closed', () => {
     }
 })
 
-app.on('activate', () => {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
-    if (BrowserWindow.getAllWindows().length === 0) {
-        createMainWindow()
-    }
-});
+// app.on('activate', () => {
+//     // On macOS it's common to re-create a window in the app when the
+//     // dock icon is clicked and there are no other windows open.
+//     if (BrowserWindow.getAllWindows().length === 0) {
+//         createMainWindow()
+//     }
+// });
 
 if (process.env.NODE_ENV !== 'production') {
     app.commandLine.appendSwitch('remote-debugging-port', '9222');
