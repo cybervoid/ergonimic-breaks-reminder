67536e136d3d3aab8d72f6295b9cdd4e1c138b8c
Rafael Gil <rafaelgil83@gmail.com>
2020-06-03 12:05:27 -0400

    - refactored and created break controller

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
 const electron = require('electron');
+const BreaksController = require('./lib/BreaksController');
 
 const {app, BrowserWindow, Menu, ipcMain, Tray} = electron;
 const WindowController = require('./lib/WindowController');
+const breaksController = new BreaksController();
+
 
 const windowController = new WindowController(BrowserWindow, app);
 
-let mainWindow, settingsWindow, tray, timer;
+let breakWindow, settingsWindow, tray, timer;
 const isMac = process.platform === 'darwin';
 const timerDuration = 40; //in minutes
 
 // process.env.NODE_ENV = 'production';
+if (process.env.NODE_ENV !== 'production') {
+    app.commandLine.appendSwitch('remote-debugging-port', '9222');
+}
 
 function createTray() {
     tray = new Tray('src/assets/img/icons/tray_icons/IconTemplate.png')
@@ -38,66 +44,43 @@ function createTray() {
     tray.setContextMenu(contextMenu)
 }
 
-function startTimer() {
+// function startTimer() {
+//
+//     let duration = timerDuration * 60 * 1000;
+//     let countDownDate = new Date(Date.now() + (duration)).getTime();
+//     let i = 0;
+//
+//     timer = setInterval(() => {
+//         // Get today's date and time
+//         let now = new Date().getTime();
+//
+//         // Find the distance between now and the count down date
+//         let distance = countDownDate - now;
+//         i++;
+//
+//         // Time calculations for days, hours, minutes and seconds
+//         // var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+//         // var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+//         let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+//         let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+//
+//         minutes = ('0' + minutes).slice(-2)
+//         seconds = ('0' + seconds).slice(-2);
+//
+//         if (distance <= 1) {
+//             clearInterval(timer);
+//             breakWindow = windowController.createBreakWindow();
+//         } else {
+//             tray.setTitle(`${minutes}:${seconds}`);
+//         }
+//     }, 1000)
+// }
 
-    let duration = timerDuration * 60 * 1000;
-    let countDownDate = new Date(Date.now() + (duration)).getTime();
-    let i = 0;
-
-    timer = setInterval(() => {
-        // Get today's date and time
-        let now = new Date().getTime();
-
-        // Find the distance between now and the count down date
-        let distance = countDownDate - now;
-        i++;
-
-        // Time calculations for days, hours, minutes and seconds
-        // var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-        // var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-        let seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-        minutes = ('0' + minutes).slice(-2)
-        seconds = ('0' + seconds).slice(-2);
-
-        if (distance <= 1) {
-            clearInterval(timer);
-            windowController.createBreakWindow();
-        } else {
-            tray.setTitle(`${minutes}:${seconds}`);
-        }
-    }, 1000)
+function processMainTimer(label) {
+    tray.setTitle(label);
 }
-
-//catch item:add
-ipcMain.on('item:add', (e, item) => {
-    mainWindow.webContents.send('item:add', item);
-    settingsWindow.close();
-})
 
 app.whenReady().then(() => {
     createTray();
-    startTimer();
+    breaksController.createTimer(timerDuration, processMainTimer);
 });
-
-// Quit when all windows are closed.
-app.on('window-all-closed', () => {
-    // On macOS it is common for applications and their menu bar
-    // to stay active until the user quits explicitly with Cmd + Q
-    if (process.platform !== 'darwin') {
-        app.quit()
-    }
-})
-
-// app.on('activate', () => {
-//     // On macOS it's common to re-create a window in the app when the
-//     // dock icon is clicked and there are no other windows open.
-//     if (BrowserWindow.getAllWindows().length === 0) {
-//         createMainWindow()
-//     }
-// });
-
-if (process.env.NODE_ENV !== 'production') {
-    app.commandLine.appendSwitch('remote-debugging-port', '9222');
-}
---
+++ src/lib/BreaksController.js
@@ -0,0 +1,39 @@
+module.exports = class BreaksController {
+
+    /**
+     *
+     * @param timerDuration in minutes
+     * @param callback
+     */
+    createTimer(timerDuration, callback) {
+        let duration = timerDuration * 60 * 1000;
+        let countDownDate = new Date(Date.now() + (duration)).getTime();
+        let i = 0;
+
+        return setInterval(() => {
+            // Get today's date and time
+            let now = new Date().getTime();
+
+            // Find the distance between now and the count down date
+            let distance = countDownDate - now;
+            i++;
+
+            // Time calculations for days, hours, minutes and seconds
+            // var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+            // var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+            let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+            let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+            minutes = ('0' + minutes).slice(-2)
+            seconds = ('0' + seconds).slice(-2);
+
+            if (distance <= 1) {
+                clearInterval(timer);
+                breakWindow = windowController.createBreakWindow();
+            } else {
+                const timerLabel = `${minutes}:${seconds}`;
+                callback(timerLabel);
+            }
+        }, 1000)
+    }
+}
